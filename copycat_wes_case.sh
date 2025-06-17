571f4edf9842f5b5f09e6c22bcbd3db79ecf5990
djordje <djordje5996@gmail.com>
2025-06-17 13:24:19 +0200

    Changed copycat_wes_case to have cohort

--- copycat_wes_case.sh
+++ copycat_wes_case.sh
@@ -13,14 +13,21 @@ module load miniconda3
 
 set -ex
 
+# RUN VARIABLES
 SAMPLE=$1
+COHORT=$2
 WDIR="/lustre/imgge/RFZO"
-REF=$WDIR/refs/hg38.fasta
+THREADS=${SLURM_CPUS_PER_TASK}
+
+# REFERENCE VARIABLES
+REF="${WDIR}/refs/hg38.fasta"
+DBSNP="/lustre/imgge/db/hg38/hg38.dbsnp155.vcf.gz"
+INTERVALS="${WDIR}/refs/hg38_Twist_ILMN_Exome_2.0_Plus_Panel_Combined_Mito.bed"
 
 mkdir -p \
-  output/${SAMPLE}/bams/metrics \
-  output/${SAMPLE}/vcfs \
-  output/${SAMPLE}/counts
+  output/${COHORT}/${SAMPLE}/bams/metrics \
+  output/${COHORT}/${SAMPLE}/vcfs \
+  output/${COHORT}/${SAMPLE}/counts
 
 eval "$(conda shell.bash hook)"
 conda activate gatk
@@ -29,18 +36,18 @@ gatk CollectReadCounts \
   -R ${REF} \
   -L ${WDIR}/refs/read_counts_wes.interval_list \
   -imr OVERLAPPING_ONLY \
-  -I ${WDIR}/input/${SAMPLE}.bam \
-  -O ${WDIR}/output/${SAMPLE}/counts/${SAMPLE}.hdf5
+  -I ${WDIR}/output/${COHORT}/${SAMPLE}/bams/${SAMPLE}.bam \
+  -O ${WDIR}/output/${COHORT}/${SAMPLE}/counts/${SAMPLE}.hdf5
 
 echo "Finished counting reads!"
 
-SCATTERS=$(basename ${WDIR}/output/WES250203/interval_scatters/temp_0001_of_* \
+SCATTERS=$(basename ${WDIR}/output/WES250529/interval_scatters/temp_0001_of_* \
   | cut -d "_" -f 4)
 
 gatk DetermineGermlineContigPloidy \
-  --model ${WDIR}/output/WES250203/ploidy-model/ \
-  -I ${WDIR}/output/${SAMPLE}/counts/${SAMPLE}.hdf5 \
-  -O ${WDIR}/output/${SAMPLE}/ \
+  --model ${WDIR}/output/WES250529/ploidy-model/ \
+  -I ${WDIR}/output/${COHORT}/${SAMPLE}/counts/${SAMPLE}.hdf5 \
+  -O ${WDIR}/output/${COHORT}/${SAMPLE}/ \
   --output-prefix ploidy
 
 echo "Finished determining ploidy!"
@@ -49,53 +56,68 @@ for SCATTER in $(seq -w 0001 00${SCATTERS})
 do
   gatk GermlineCNVCaller \
     --run-mode CASE \
-    --model ${WDIR}/output/WES250203/gcnvcaller_scatters/scatter_${SCATTER}-model \
-    -I ${WDIR}/output/${SAMPLE}/counts/${SAMPLE}.hdf5 \
-    -O ${WDIR}/output/${SAMPLE}/gcnvcaller_scatters \
+    --model ${WDIR}/output/WES250529/gcnvcaller_scatters/scatter_${SCATTER}-model \
+    -I ${WDIR}/output/${COHORT}/${SAMPLE}/counts/${SAMPLE}.hdf5 \
+    -O ${WDIR}/output/${COHORT}/${SAMPLE}/gcnvcaller_scatters \
     --output-prefix scatter_${SCATTER} \
-    --contig-ploidy-calls ${WDIR}/output/${SAMPLE}/ploidy-calls \
-    --verbosity DEBUG
+    --contig-ploidy-calls ${WDIR}/output/${COHORT}/${SAMPLE}/ploidy-calls \
+    --verbosity DEBUG &
 done
+wait
 
 echo "Finished calling CNVs per scatter"
 
-MODELS=$(ls -p ${WDIR}/output/WES250203/gcnvcaller_scatters/ \
+MODELS=$(ls -p ${WDIR}/output/WES250529/gcnvcaller_scatters/ \
   | grep model \
-  | sed "s#^#--model-shard-path ${WDIR}/output/WES250203/gcnvcaller_scatters/#g")
-CALLS=$(ls -p ${WDIR}/output/${SAMPLE}/gcnvcaller_scatters/ \
+  | sed "s#^#--model-shard-path ${WDIR}/output/WES250529/gcnvcaller_scatters/#g")
+CALLS=$(ls -p ${WDIR}/output/${COHORT}/${SAMPLE}/gcnvcaller_scatters/ \
   | grep calls \
-  | sed "s#^#--calls-shard-path ${WDIR}/output/${SAMPLE}/gcnvcaller_scatters/#g")
-
+  | sed "s#^#--calls-shard-path ${WDIR}/output/${COHORT}/${SAMPLE}/gcnvcaller_scatters/#g")
 
 gatk PostprocessGermlineCNVCalls \
   $MODELS \
   $CALLS \
   --sample-index 0 \
-  --output-genotyped-intervals ${WDIR}/output/${SAMPLE}/vcfs/${SAMPLE}_intervals.cnv.vcf.gz \
-  --output-genotyped-segments ${WDIR}/output/${SAMPLE}/vcfs/${SAMPLE}_raw.cnv.vcf.gz \
-  --output-denoised-copy-ratios ${WDIR}/output/${SAMPLE}/gcnvcaller_scatters/${SAMPLE}_denoised_copy_ratios.tsv \
-  --contig-ploidy-calls ${WDIR}/output/${SAMPLE}/ploidy-calls/ \
+  --output-genotyped-intervals ${WDIR}/output/${COHORT}/${SAMPLE}/vcfs/${SAMPLE}_intervals.cnv.vcf.gz \
+  --output-genotyped-segments ${WDIR}/output/${COHORT}/${SAMPLE}/vcfs/${SAMPLE}_raw.cnv.vcf.gz \
+  --output-denoised-copy-ratios ${WDIR}/output/${COHORT}/${SAMPLE}/gcnvcaller_scatters/${SAMPLE}_denoised_copy_ratios.tsv \
+  --contig-ploidy-calls ${WDIR}/output/${COHORT}/${SAMPLE}/ploidy-calls/ \
   --allosomal-contig chrX --allosomal-contig chrY \
   --sequence-dictionary ${WDIR}/refs/hg38.dict
 
-echo "Finished calling CNVs per sample"
+echo "Finished processing CNVs"
 
 gatk VariantFiltration \
-  -V ${WDIR}/output/${SAMPLE}/vcfs/${SAMPLE}_raw.cnv.vcf.gz \
-  -filter "QUAL < 30.0" \
-  --filter-name "CNVQUAL" \
-  -O ${WDIR}/output/${SAMPLE}/vcfs/${SAMPLE}_filtered.cnv.vcf.gz
+  -V ${WDIR}/output/${COHORT}/${SAMPLE}/vcfs/${SAMPLE}_raw.cnv.vcf.gz \
+  -filter "QUAL < 100.0" --filter-name "CNVQUAL" \
+  -filter "QUAL < 30.0" --filter-name "CNVRMV" \
+  -O ${WDIR}/output/${COHORT}/${SAMPLE}/vcfs/${SAMPLE}_filtered.cnv.vcf.gz
 
 echo "Finished filtering CNV calls"
 
-zgrep -P -v "CNVQUAL|N\t\." ${WDIR}/output/${SAMPLE}/vcfs/${SAMPLE}_filtered.cnv.vcf.gz \
-  | sed -e 's/##source=VariantFiltration/##source=VariantFiltration\n##reference=hg38.fasta/g' \
-    -e 's/\tEND/\tSVTYPE=CNV;END/g' \
-  | bgzip -o ${WDIR}/output/${SAMPLE}/vcfs/${SAMPLE}.cnv.vcf.gz
-tabix ${WDIR}/output/${SAMPLE}/vcfs/${SAMPLE}.cnv.vcf.gz
+zgrep -P -v "CNVRMV|N\t\." ${WDIR}/output/${COHORT}/${SAMPLE}/vcfs/${SAMPLE}_filtered.cnv.vcf.gz \
+  | sed 's/\tEND/\tSVTYPE=CNV;END/g' \
+  | bgzip -o ${WDIR}/output/${COHORT}/${SAMPLE}/vcfs/${SAMPLE}.cnv.vcf.gz
+tabix ${WDIR}/output/${COHORT}/${SAMPLE}/vcfs/${SAMPLE}.cnv.vcf.gz
 
 echo "Done with CNVs!"
 
-rm ${WDIR}/output/${SAMPLE}/bams/*_* ${WDIR}/output/${SAMPLE}/vcfs/*_*
+configManta.py \
+  --referenceFasta ${REF} \
+  --callRegions refs/wes_manta.bed.gz \
+  --bam ${WDIR}/output/${COHORT}/${SAMPLE}/bams/${SAMPLE}.bam \
+  --runDir ${WDIR}/output/${COHORT}/${SAMPLE}/manta/ \
+  --exome
+  
+${WDIR}/output/${COHORT}/${SAMPLE}/manta/runWorkflow.py -j ${THREADS}
+
+mv ${WDIR}/output/${COHORT}/${SAMPLE}/manta/results/variants/diploidSV.vcf.gz \
+  ${WDIR}/output/${COHORT}/${SAMPLE}/vcfs/${SAMPLE}.sv.vcf.gz
+mv ${WDIR}/output/${COHORT}/${SAMPLE}/manta/results/variants/diploidSV.vcf.gz.tbi \
+  ${WDIR}/output/${COHORT}/${SAMPLE}/vcfs/${SAMPLE}.sv.vcf.gz.tbi
+
+echo "Done with SVs!"
+
+rm ${WDIR}/output/${COHORT}/${SAMPLE}/bams/*_* ${WDIR}/output/${COHORT}/${SAMPLE}/vcfs/*_*
 
 echo "All Done!"
